d272344477868ebd6f799f5fd16dac0ef6384b4f
moons <moons@featuring.in>
2021-02-03 03:22:16 +0900

    url

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url
+from django.shortcuts import redirect
 
 from django.conf import settings
 from django.conf.urls.static import static
@@ -8,6 +9,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     # 어드민
     path('admin/', admin.site.urls),
+    # 그냥 홈
+    path('', lambda request: redirect('thebible/read/', permanent=True)),
     # 블로그 관련
     path('blog/', include('myblog.urls')),
     # 성경관련
