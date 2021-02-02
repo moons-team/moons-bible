e5dd518192da4f9a1bdcb3e75dde5a22c234ac5f
moons <moons@featuring.in>
2021-02-03 02:22:25 +0900

    서브모듈 삭제

---
+++ mysite/urls.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls import url
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    # 어드민
+    path('admin/', admin.site.urls),
+    # 블로그 관련
+    path('blog/', include('myblog.urls')),
+    # 성경관련
+    path('thebible/', include('thebible.urls')),
+    # 유저페이지
+    path('users/', include('users.urls')),
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
